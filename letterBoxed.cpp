9f6c1ef66748df71257c5dec24b80fac1654087e
satsinush <aneedham0577@gmail.com>
2024-10-01 10:56:29 -0500

    commit 2

--- letterBoxed.cpp
+++ letterBoxed.cpp
@@ -73,11 +73,11 @@ vector<string> filterWords(vector<string>& words, vector<string>& sides){
     return(newWords);
 }
 
-int stringLength(string word){
+int stringLength(string& word){
     return(word.length());
 }
 
-vector<string> sortWords(vector<string>& words, int (*sorter)(string)){
+vector<string> sortWords(vector<string>& words, int (*sorter)(string&), bool ascending=true){
     if(words.size() <= 1){
         return(words);
     }
@@ -98,21 +98,69 @@ vector<string> sortWords(vector<string>& words, int (*sorter)(string)){
             less.push_back(w);
         }
     }
-    less = sortWords(less, sorter);
-    greater = sortWords(greater, sorter);
-    less.insert(less.begin(), equal.begin(), equal.end());
-    less.insert(less.begin(), greater.begin(), greater.end());
-    return(less);
+    less = sortWords(less, sorter, ascending);
+    greater = sortWords(greater, sorter, ascending);
+    vector<string> sorted = {};
+    if(ascending){
+        sorted.insert(sorted.end(), less.begin(), less.end());
+        sorted.insert(sorted.end(), equal.begin(), equal.end());
+        sorted.insert(sorted.end(), greater.begin(), greater.end());
+    }else{
+        sorted.insert(sorted.end(), greater.begin(), greater.end());
+        sorted.insert(sorted.end(), equal.begin(), equal.end());
+        sorted.insert(sorted.end(), less.begin(), less.end());
+    }
+    return(sorted);
 }
 
-bool matches(string a, string b){
+int listLength(vector<string>& list){
+    return(list.size());
+}
+
+vector<vector<string>> sortList(vector<vector<string>>& list, int (*sorter)(vector<string>&), bool ascending=true){
+    if(list.size() <= 1){
+        return(list);
+    }
+
+    int pivot = sorter(list[0]);
+    vector<vector<string>> less = {};
+    vector<vector<string>> equal = {list[0]};
+    vector<vector<string>> greater = {};
+    for(int i = 1; i < list.size(); i++){
+        vector<string> w = list[i];
+        if(sorter(w) == pivot){
+            equal.push_back(w);
+        }
+        if(sorter(w) > pivot){
+            greater.push_back(w);
+        }
+        if(sorter(w) < pivot){
+            less.push_back(w);
+        }
+    }
+    less = sortList(less, sorter, ascending);
+    greater = sortList(greater, sorter, ascending);
+    vector<vector<string>> sorted = {};
+    if(ascending){
+        sorted.insert(sorted.end(), less.begin(), less.end());
+        sorted.insert(sorted.end(), equal.begin(), equal.end());
+        sorted.insert(sorted.end(), greater.begin(), greater.end());
+    }else{
+        sorted.insert(sorted.end(), greater.begin(), greater.end());
+        sorted.insert(sorted.end(), equal.begin(), equal.end());
+        sorted.insert(sorted.end(), less.begin(), less.end());
+    }
+    return(sorted);
+}
+
+bool matches(string& a, string& b){
     if(a == ""){
         return(true);
     }
     return(a[a.size()-1] == b[0]);
 }
 
-void printList(vector<string> v){
+void printList(vector<string>& v){
     for(string& s: v){
         cout << s << " ";
     }
@@ -129,31 +177,24 @@ bool containsAllLetters(vector<string>& v, vector<string>& letters){
     return(true);
 }
 
-vector<vector<string>> getValidString(vector<string>& words, string last, vector<string> sides, int depth = 0, int maxDepth = 5){    
+vector<vector<string>> getValidString(vector<string>& words, string& last, vector<string>& sides, int depth = 0, int maxDepth = 5){    
     if(words.size() == 0 || depth+1 > maxDepth){
-        return(vector<vector<string>>{{}});
+        return(vector<vector<string>>{});
     }
     
     vector<vector<string>> validStrings = {};
     for(int i = 0; i < words.size(); i++){
         string w = words[i];
         if(matches(last, w)){
-
             vector<string> newWords(words.begin(),words.begin()+i);
             newWords.insert(newWords.end(),words.begin()+i+1,words.end());
-            bool longerList = false;
+            
+            validStrings.push_back({w}); 
             vector<string> v;
             for(vector<string> s: getValidString(newWords, w, sides, depth+1, maxDepth)){
-                longerList = true;
                 v = {w};
                 v.insert(v.end(),s.begin(),s.end());
                 validStrings.push_back(v);
-                
-            }
-            if(!longerList){
-                v = {w};
-                validStrings.push_back(v);
-                
             }
         }
     }
@@ -172,6 +213,9 @@ vector<vector<string>> filterLists(vector<vector<string>>& l, vector<string>& si
 }
 
 int main(){
+
+    cout << "Reading word file\n";
+
     ifstream file("words.txt");
 
     vector<string> words = {};
@@ -186,37 +230,41 @@ int main(){
     string testWord = "apple";
 
 
-    string top = "ozu";
-    string bottom = "nri";
-    string left = "lfa";
-    string right = "mbt";
+    string top = "dth";
+    string bottom = "ofs";
+    string left = "ban";
+    string right = "wri";
 
-    /*cout << "Top\n";
+    /*
+    cout << "Top\n";
     cin >> top;
     cout << "\nBottom\n";
     cin >> bottom;
     cout << "\nLeft\n";
     cin >> left;
     cout << "\nRight\n";
-    cin >> right;*/
+    cin >> right;
+    */
 
     vector<string> allSides = {top, bottom, left, right};
 
+    cout << "\nFiltering word list for valid words\n";
     words = filterWords(words, allSides);
-    words = sortWords(words, stringLength);
+    cout << "\nSorting word list\n";
+    words = sortWords(words, stringLength, false);
 
-    cout << "\n\n";
+    int maxDepth = 3;
 
-    //words = vector<string>(words.begin(),words.begin()+50);
-
-    for(string& word: words){
-        cout << word << "\n";
-    }cout << "\n\n";
-
-    vector<vector<string>> allLists = getValidString(words, "", allSides, 0, 4);
+    cout << "\nSearching for all possible word strings (Max length = " << maxDepth << ")\n";
+    string last = "";
+    vector<vector<string>> allLists = getValidString(words, last, allSides, 0, maxDepth);
+    cout << "\nFiltering string list\n";
+    allLists = filterLists(allLists, allSides);
+    cout << "\nSorting string list\n";
+    allLists = sortList(allLists, listLength, false);
     
     
-    for(vector<string>& v: filterLists(allLists, allSides)){
+    for(vector<string>& v: allLists){
         for(string& s: v){
             cout << s << " ";
         }
